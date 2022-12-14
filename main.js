7fdf7b32b189b2e4705472bd18457e2af7b8b736
jgwDesign <jaiofjgw@gmail.com>
2022-12-14 00:41:31 -0500

    General Updates

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const timeHTML = document.getElementById("time");
 const dateHTML = document.getElementById("date");
 
-function updateTimeAndDate() {
+let now = new Date();
+timeHTML.innerText = now.toLocaleTimeString('en-US', { 
+    timeZone: 'America/New_York',
+    hour: '2-digit',
+    minute: '2-digit'
+}).toLowerCase().replaceAll(':', ' ') + " EST";
+
+dateHTML.innerText = now.toLocaleDateString('en-US', { 
+    timeZone: 'America/New_York'
+}).toLowerCase().replaceAll('/', ' ');
+
+setInterval(function() {
     let now = new Date();
     timeHTML.innerText = now.toLocaleTimeString('en-US', { 
         timeZone: 'America/New_York',
         hour: '2-digit',
         minute: '2-digit'
     }).toLowerCase().replaceAll(':', ' ') + " EST";
-
-    dateHTML.innerText = now.toLocaleDateString('en-US', { 
-        timeZone: 'America/New_York'
-    }).toLowerCase().replaceAll('/', ' ');
-}
-
-updateTimeAndDate();
-setInterval(updateTimeAndDate, 1000);
+}, 1000);
