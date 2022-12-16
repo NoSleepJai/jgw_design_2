b91630bd45919e248e7db1fbe9c94442bcb26bf4
jgwDesign <jaiofjgw@gmail.com>
2022-12-16 02:28:49 -0500

    JS Updates

--- main.js
+++ main.js
@@ -1,22 +1,62 @@
 const timeHTML = document.getElementById("time");
 const dateHTML = document.getElementById("date");
 
-let now = new Date();
-timeHTML.innerText = now.toLocaleTimeString('en-US', { 
-    timeZone: 'America/New_York',
-    hour: '2-digit',
-    minute: '2-digit'
-}).toLowerCase().replaceAll(':', ' ') + " EST";
 
-dateHTML.innerText = now.toLocaleDateString('en-US', { 
-    timeZone: 'America/New_York'
-}).toLowerCase().replaceAll('/', ' ');
-
-setInterval(function() {
+function updateTimeAndDate() {
     let now = new Date();
     timeHTML.innerText = now.toLocaleTimeString('en-US', { 
         timeZone: 'America/New_York',
         hour: '2-digit',
         minute: '2-digit'
-    }).toLowerCase().replaceAll(':', ' ') + " EST";
+    }).toLowerCase().replace(':', ' ') + " EST";
+    
+    dateHTML.innerText = now.toLocaleDateString('en-US', { 
+        timeZone: 'America/New_York'
+    }).toLowerCase().replace(/\//g, ' ');
+}
+
+
+function updateTheme() {
+    let now = new Date();
+    if (now.getHours() >= 18 || now.getHours() < 7) {
+        document.body.classList.add("night");
+        document.body.classList.remove("day");
+    } else {
+        document.body.classList.add("day");
+        document.body.classList.remove("night");
+    }
+}
+var current;
+function revealProject(nth) {
+    let project = document.querySelectorAll(`.flex-container--right .row:nth-of-type(${nth})`);
+    let otherProject = document.querySelectorAll(`.flex-container--right .row:not(:nth-of-type(${nth}))`);
+    // console.log(project);
+    // console.log(otherProject);
+    if (current == nth && project.item(0).style.height != "25%") {
+        otherProject.forEach(
+            function(e) {
+                e.style.height = "25%";
+            }
+        )
+        project.item(0).style.height = "25%";
+    }
+    else {
+        otherProject.forEach(
+            function(e) {
+                e.style.height = "3.007%";
+            }
+        )
+        project.item(0).style.height = "75%";
+    }
+
+    current = nth;
+
+}
+
+updateTimeAndDate();
+updateTheme();
+
+setInterval(function() {
+    updateTimeAndDate();
 }, 1000);
+
