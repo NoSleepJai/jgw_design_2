376ea9e428192d73535839af6db865066845fc9f
jgwDesign <jaiofjgw@gmail.com>
2022-12-20 13:37:23 -0500

    Added images, updated font

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const timeHTML = document.getElementById("time");
 const dateHTML = document.getElementById("date");
 
 
+
 function updateTimeAndDate() {
     let now = new Date();
     timeHTML.innerText = now.toLocaleTimeString('en-US', { 
@@ -16,6 +17,7 @@ function updateTimeAndDate() {
 }
 
 
+
 function updateTheme() {
     let now = new Date();
     if (now.getHours() >= 18 || now.getHours() < 7) {
@@ -26,25 +28,38 @@ function updateTheme() {
         document.body.classList.remove("night");
     }
 }
+
+
+
 var current;
 function revealProject(nth) {
     let project = document.querySelector(`.flex-container--right .row:nth-of-type(${nth})`);
+    let projectContent = document.querySelector(`.flex-container--right .row:nth-of-type(${nth}) .project-content`);
     let otherProject = document.querySelectorAll(`.flex-container--right .row:not(:nth-of-type(${nth}))`);
-    // console.log(project);
-    // console.log(otherProject);
+    let otherProjectContent = document.querySelectorAll(`.flex-container--right .row:not(:nth-of-type(${nth})) .project-content`);
+
+    // check if project is already opened if it is collapse back to the original layout
     if (current == nth && project.style.height != "25%") {
         otherProject.forEach((e) => { e.style.height = "25%"; })
         project.style.height = "25%";
+        // project.innerHTML = "(" + project.innerHTML + ")";
+        projectContent.style.opacity = "0%";
     } else {
         otherProject.forEach((e) => { e.style.height = "3.007%"; })
+        otherProjectContent.forEach((e) => { e.style.opacity = "0%"; })
         project.style.height = "75%";
+        projectContent.style.opacity = "100%";
     }
 
     current = nth;
 }
 
+
+
+
 updateTimeAndDate();
 updateTheme();
+
 
 setInterval(function() {
     updateTimeAndDate();
